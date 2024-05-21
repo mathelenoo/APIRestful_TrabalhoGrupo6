f0e73370ca9dd4b17e628015f8d4cf349f084896
mathelenoo <helenomath@gmail.com>
2024-05-21 11:25:47 -0300

    commit: criação do projeto maven e criacao de pacotes

---
+++ G6_API_TrabalhoFinal/src/main/java/org/serratec/grupo6/social/G6ApiTrabalhoFinalApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.grupo6.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G6ApiTrabalhoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G6ApiTrabalhoFinalApplication.class, args);
+	}
+
+}
---
+++ G6_API_TrabalhoFinal/src/test/java/org/serratec/grupo6/social/G6ApiTrabalhoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.grupo6.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G6ApiTrabalhoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
