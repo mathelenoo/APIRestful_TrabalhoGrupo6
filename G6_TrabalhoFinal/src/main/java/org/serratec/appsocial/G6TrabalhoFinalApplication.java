744af269fb7cbcb5f2f196c6c4d036794c0aeea8
mathelenoo <helenomath@gmail.com>
2024-05-21 15:01:57 -0300

    commit: criacao de classes, repositorio e criacao do banco

---
+++ G6_TrabalhoFinal/src/main/java/org/serratec/appsocial/G6TrabalhoFinalApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.appsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G6TrabalhoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G6TrabalhoFinalApplication.class, args);
+	}
+
+}
---
+++ G6_TrabalhoFinal/src/test/java/org/serratec/appsocial/G6TrabalhoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.appsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G6TrabalhoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
