be035f5220a9e39f4ced164882ca7e69eb41ce58
MatheusARodrigues <matheusadrianor7@gmail.com>
2024-05-22 18:16:10 -0300

    Feat: Subindo as classes Controller de Postagem e Comentario.

--- G6_TrabalhoFinal/src/main/java/org/serratec/appsocial/controller/ComentarioController.java
+++ G6_TrabalhoFinal/src/main/java/org/serratec/appsocial/controller/ComentarioController.java
@@ -1,15 +1,57 @@
 package org.serratec.appsocial.controller;
 
-import org.serratec.appsocial.service.UsuarioService;
+import java.util.List;
+import java.util.Optional;
+
+import org.serratec.appsocial.service.ComentarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("/usuarios")
+@RequestMapping ("/comentarios")
 public class ComentarioController {
 	
 	@Autowired
-	private UsuarioService usuarioService;
+	private ComentarioService comentarioService;
+	
+	GetMapping 
+	public ResponseEntity<List<Comentario>> listar() {
+		return ResponseEntity.ok(comentarioRepository.findAll());
+	}
+
+	@GetMapping("/{id}") 
+	public ResponseEntity<Comentario> buscar(@PathVariable Long id) {
+		Optional<Comentario> livroOpt = comentarioRepository.findById(id);
+		if (livroOpt.isPresent()) {
+			return ResponseEntity.ok(livroOpt.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping 
+	@ResponseStatus(HttpStatus.CREATED)
+	public Comentario createLivro(@Valid @RequestBody Comentario comentario) {
+		return comentarioRepository.save(comentario);
+	}
+
+	@PutMapping("/{id}") 
+	public ResponseEntity<Comentario> atualizar(@PathVariable Long id, @Valid @RequestBody Comentario comentario) {
+		if (!comentarioRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		comentario.setId(id);
+		comentario = comentarioRepository.save(comentario);
+		return ResponseEntity.ok(comentario);
+	}
+
+	@DeleteMapping("/{id}") 
+	public ResponseEntity<Void> remover(@PathVariable Long id) {
+		if (!comentarioRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		comentarioRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
--- G6_TrabalhoFinal/src/main/java/org/serratec/appsocial/controller/PostagemController.java
+++ G6_TrabalhoFinal/src/main/java/org/serratec/appsocial/controller/PostagemController.java
@@ -1,5 +1,50 @@
 package org.serratec.appsocial.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository postagemRepository;
+
+	@GetMapping // Método para Listar todos os Usuários
+	public ResponseEntity<List<Postagem>> listar() {
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+
+	@GetMapping("/{id}") // Método para listar usuário por ID
+	public ResponseEntity<Postagem> buscar(@PathVariable Long id) {
+		Optional<Postagem> livroOpt = postagemRepository.findById(id);
+		if (livroOpt.isPresent()) {
+			return ResponseEntity.ok(livroOpt.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping // Método para criar um novo usuário
+	@ResponseStatus(HttpStatus.CREATED)
+	public Postagem createLivro(@Valid @RequestBody Postagem postagem) {
+		return postagemRepository.save(postagem);
+	}
+
+	@PutMapping("/{id}") // Método para atualizar um usuário
+	public ResponseEntity<Postagem> atualizar(@PathVariable Long id, @Valid @RequestBody Postagem postagem) {
+		if (!postagemRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		postagem.setId(id);
+		postagem = postagemRepository.save(postagem);
+		return ResponseEntity.ok(postagem);
+	}
+
+	@DeleteMapping("/{id}") // Método para deletar um usuário do banco
+	public ResponseEntity<Void> remover(@PathVariable Long id) {
+		if (!postagemRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		postagemRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
