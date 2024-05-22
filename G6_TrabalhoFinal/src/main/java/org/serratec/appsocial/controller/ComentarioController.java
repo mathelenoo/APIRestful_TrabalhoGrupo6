b0bc1546aec69d2d5b8f8414be454569cff81cfc
MatheusARodrigues <matheusadrianor7@gmail.com>
2024-05-22 18:27:36 -0300

    Fix: Consertando imports nas classes controller

--- G6_TrabalhoFinal/src/main/java/org/serratec/appsocial/controller/ComentarioController.java
+++ G6_TrabalhoFinal/src/main/java/org/serratec/appsocial/controller/ComentarioController.java
@@ -3,19 +3,31 @@ package org.serratec.appsocial.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.serratec.appsocial.service.ComentarioService;
+import org.serratec.appsocial.model.Comentario;
+import org.serratec.appsocial.repository.ComentarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping ("/comentarios")
 public class ComentarioController {
 	
 	@Autowired
-	private ComentarioService comentarioService;
+	private ComentarioRepository comentarioRepository;
 	
-	GetMapping 
+	@GetMapping 
 	public ResponseEntity<List<Comentario>> listar() {
 		return ResponseEntity.ok(comentarioRepository.findAll());
 	}
--- G6_TrabalhoFinal/src/main/java/org/serratec/appsocial/controller/PostagemController.java
+++ G6_TrabalhoFinal/src/main/java/org/serratec/appsocial/controller/PostagemController.java
@@ -3,6 +3,21 @@ package org.serratec.appsocial.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.serratec.appsocial.model.Postagem;
+import org.serratec.appsocial.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import jakarta.validation.Valid;
+
 public class PostagemController {
 	
 	@Autowired
